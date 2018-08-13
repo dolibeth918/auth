2dce73fc4f498c9ebdf2aa99f7e1240d3ddb2e13
Dolibeth Estevez <dolibeth.estevez@gmail.com>
2018-08-13 17:46:42 -0400

    add(app): add firebase

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Header } from './components/common';
+import firebase from './firebase';
 
 class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyDFvnu1cxKYewSzGcWHw5epV_TDEfdyE5A',
+      authDomain: 'auth-9f5a4.firebaseapp.com',
+      databaseURL: 'https://auth-9f5a4.firebaseio.com',
+      projectId: 'auth-9f5a4',
+      storageBucket: 'auth-9f5a4.appspot.com',
+      messagingSenderId: '157213089576'
+    };
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <View>
-        <Text>What</Text>
+        <Header headerText="Authentication" />
       </View>
     );
   }
